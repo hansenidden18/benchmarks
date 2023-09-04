aa9de80f60de0ec56a7b87c51ae3dfae825da0e7
hansenidden <hansenidden18@gmail.com>
2023-09-04 10:48:31 +0000

    Add write-first.sh for writing the storage first

--- fio/benchmark.sh
+++ fio/benchmark.sh
@@ -6,7 +6,7 @@ SOURCE="${BASH_SOURCE[0]}"
 TOPDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 DIR=$TOPDIR/result
-NUMJOBS=(1 2 4 8 16 32 64 128)
+NUMJOBS=(1)
 #BLOCK_SIZE=("4k" "16k" "64k" "256k")
 BLOCK_SIZE=("4k")
 
@@ -25,17 +25,11 @@ MQ_SIZE=$(echo "scale=0;$DEV_ZONE_SIZE*$DEV_SECT_SIZE*$DEV_ZONES" | bc)
 mkdir -p $DIR
 cd $DIR
 
-blkzone reset /dev/nvme0n1
-
-echo mq-deadline | tee /sys/block/nvme0n1/queue/scheduler > /dev/null
-echo "Filling entire namespace for read benchs"
-fio --name=zns-fio --filename=/dev/nvme0n1 --direct=1 --size=$MQ_SIZE --ioengine=libaio --iodepth=4 --rw=write --bs=512K --zonemode=zbd > /dev/null
-
 for job in ${NUMJOBS[@]}; do
     echo "Running benchmarks with number of q-depths: $job"
     
     # none scheduler
     echo none | tee /sys/block/nvme0n1/queue/scheduler > /dev/null
 
-    sudo fio --name=$(echo "none_iodepth_${job}") --filename=/dev/nvme0n1 --direct=1 --size=1z --offset_increment=1z --ioengine=psync --zonemode=zbd --runtime=60 --numa_cpu_nodes=$DEV_NUMA_NODE --rw=randread --numjobs=$job --bs=$BLOCK_SIZE --time_based=1 --randrepeat=0 --norandommap=1 --group_reporting --per_job_logs=0 --stonewall --number_ios=1000 --max_open_zones=1 --write_lat_log=seq_read_lat_${job}.log > $DIR/zns_output_${job}.txt
+    sudo fio --name=$(echo "none_iodepth_${job}") --filename=/dev/nvme0n1 --direct=1 --size=100G --offset_increment=1z --ioengine=psync --zonemode=zbd --numa_cpu_nodes=$DEV_NUMA_NODE --rw=randread --numjobs=$job --bs=$BLOCK_SIZE --time_based=0 --randrepeat=0 --norandommap=1 --group_reporting --per_job_logs=0 --stonewall --number_ios=1000000 --max_open_zones=1 --write_lat_log=seq_read_lat_${job}.log > $DIR/zns_output_${job}.txt
 done
